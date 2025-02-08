9924015e43a908549204d3c379da078f64292495
shadia shirin v <vshadiyashirin2003@gmail.com>
2025-02-08 14:08:08 +0530

    requirementfile.txt

--- unirepair/login/models.py
+++ unirepair/login/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class UserDetail(models.Model):
-    UserName=models.CharField(max_length=250,null=False,unique=True)
-    Email=models.EmailField(null=False,unique=True)
-    Password=models.CharField(null=False,max_length=10)
+    UserName=models.CharField(max_length=250,unique=True)
+    Email=models.EmailField(unique=True)
+    Password=models.CharField(max_length=10)
