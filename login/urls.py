dc34f21ea7118a1e31ab660378c2ef5bad99ff67
Zenha123 <fathimazenha21@gmail.com>
2025-02-09 03:58:30 +0530

    home page added

--- login/urls.py
+++ login/urls.py
@@ -1,13 +1,11 @@
 
 from django.urls import path
-from .views import register_customer, register_agent, verify_otp, login_view
+from .views import register_customer, register_agent, verify_otp, login_view,home
 
 urlpatterns = [
     path('register/customer/', register_customer, name='register_customer'),
     path('register/agent/', register_agent, name='register_agent'),
     path('verify-otp/', verify_otp, name='verify_otp'),
     path('login/', login_view, name='login'),
-
-
-
+    path('home/',home,name='home')
 ]
--- login/views.py
+++ login/views.py
@@ -94,4 +94,5 @@ def login_view(request):
 
     return render(request, "login.html")
 
-            
+def home(request):
+    return render(request, "home.html")            
