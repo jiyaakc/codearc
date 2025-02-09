b0d32a670e12514e1789f2774fafec6759ce7c96
Jiya <jiyachandriyan@gmail.com>
2025-02-09 11:38:41 +0530

    last

--- unirepair/settings.py
+++ unirepair/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-)7@7#@%y=!lyu3*=51n6^2ec4pv3lhv1jnb^cbw=wcof#@%w7g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
